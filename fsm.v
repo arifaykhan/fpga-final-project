66962553a9d98b4e723a4c7a7aabce1d52ab8e6a
Arif Aykhan Mammadli <79666195+arifaykhan@users.noreply.github.com>
2026-01-05 16:53:50 +0400

    Update and rename fsm (2).v to fsm.v

--- fsm.v
+++ fsm.v
@@ -9,8 +9,7 @@ module fsm (
 );
     parameter PRICE = 55; 
     reg [6:0] state;
-    
-    // Registers to store the previous state of the buttons
+
     reg n_prev, d_prev, q_prev;
 
     always @(posedge clk or posedge reset) begin
@@ -18,12 +17,10 @@ module fsm (
             state <= 0;
             n_prev <= 0; d_prev <= 0; q_prev <= 0;
         end else if (enable) begin
-            // Update previous state registers
             n_prev <= nickel;
             d_prev <= dime;
             q_prev <= quarter;
 
-            // Only add money on the "Rising Edge" (when button goes 0 -> 1)
             if (nickel && !n_prev)         state <= state + 5;
             else if (dime && !d_prev)      state <= state + 10;
             else if (quarter && !q_prev)   state <= state + 25;
@@ -33,4 +30,5 @@ module fsm (
     assign dispout = (state >= PRICE) ? (state - PRICE) : state;
     assign dispense = (state >= PRICE); 
     assign collect = (state > PRICE);   
+
 endmodule
